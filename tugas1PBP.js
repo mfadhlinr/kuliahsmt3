1ceaa95eedcd544302de441e691039d0908eae7d
mfadhlinr <120742300+mfadhlinr@users.noreply.github.com>
2023-09-27 22:46:51 +0700

    Create tugas1PBP.js

---
+++ tugas1PBP.js
@@ -0,0 +1,11 @@
+let nama = "NM Fadhli NR";
+let nim = "20220040259";
+
+console.log("Nama : "+ nama);
+console.log("nim : "+ nim);
+
+let angka1 = 10;
+let angka2 = 20;
+
+let jumlah = angka1 + angka2;
+console.log("Hasil penjumlahan: " + jumlah);
