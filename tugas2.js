edd37a74f7cc45bb54dc7a2dc4f4cfbb449da8bc
mfadhlinr <120742300+mfadhlinr@users.noreply.github.com>
2023-10-04 20:25:42 +0700

    Create tugas2.js

---
+++ tugas2.js
@@ -0,0 +1,9 @@
+let firstName = "fadhli";
+let lastName = "rangkuti";
+let age = 21; 
+let isMarried = false; 
+
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Age:", age);
+console.log("Is Married:", isMarried);
